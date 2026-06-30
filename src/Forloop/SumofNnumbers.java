62bcf325efc0c9ad7354d673b1a041de0a8bfc26
Sudheer-77 <sudheerdash24@gmail.com>
2026-06-30 09:58:13 +0530

    i added 3 programs

---
+++ src/Forloop/SumofNnumbers.java
@@ -0,0 +1,20 @@
+package Forloop;
+
+import java.util.Scanner;
+
+public class SumofNnumbers {
+public static void main (String args[])
+
+{
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int sum= 0;
+	for(int i = 1;i<=20;i++)
+		
+	{
+		sum= sum+i;
+		System.out.println(sum);
+	}
+	System.out.println(sum);
+}
+}
---
+++ src/Forloop/Vowels.java
@@ -0,0 +1,16 @@
+package Forloop;
+
+public class Vowels {
+	public static void main  (String args[])
+	{
+		for (char c='a';c<='z';c++) {
+			
+		if ( c == 'a'||c=='e'||c=='i'||c=='o'||c=='u')
+		{
+			System.out.println(c);
+		}
+	}
+
+}
+}
+
