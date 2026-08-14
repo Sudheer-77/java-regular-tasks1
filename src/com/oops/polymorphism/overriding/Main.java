14e83a9b7739308c80f1cdab923560d517b2e427
Sudheer-77 <sudheerdash24@gmail.com>
2026-08-14 12:10:15 +0530

    opps

--- src/com/oops/polymorphism/overriding/Main.java
+++ src/com/oops/polymorphism/overriding/Main.java
@@ -9,5 +9,14 @@ public static void main(String []args)
 	d.work();
 	Tester T =new Tester();
 	T.work();
+	//WE CAN CREATE 2 WAYS :
+    //Employee e;
+//	 e = new Employee();
+//    e.work();
+//    e = new Developer();
+//    e.work();
+//    e = new Tester();
+//    e.work();
 }
+
 }
