a342961a1a8c61b69a35f368719dc8ea5ba14a4f
Sudheer-77 <sudheerdash24@gmail.com>
2026-08-19 21:40:54 +0530

    shallow copy deep copy

--- src/ShallowCopyAndDeepCOpy/ShallowCopy.java
+++ src/ShallowCopyAndDeepCOpy/ShallowCopy.java
@@ -1,47 +1,157 @@
 package ShallowCopyAndDeepCOpy;
 
 class Address {
+    String city;
 
-	String city;
-
-	Address(String city) {
-		this.city = city;
-	}
+    Address(String city) {
+        this.city = city;
+    }
 }
 
 class Student implements Cloneable {
 
-	int number;
-	String name;
-	Address address;
+    int number;
+    String name;
+    Address address;
 
-	Student(int number, String name, Address address) {
-		this.number = number;
-		this.name = name;
-		this.address = address;
-	}
+    Student(int number, String name, Address address) {
+        this.number = number;
+        this.name = name;
+        this.address = address;
+    }
 
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
+    // Shallow Copy
+    @Override
+    protected Student clone() throws CloneNotSupportedException {
+        return (Student) super.clone();
+    }
 }
 
 public class ShallowCopy {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
-		Address a = new Address("Hyderabad");
+        // Create Address object
+        Address address = new Address("Vizag");
 
-		Student s1 = new Student(7, "Sudheer", a);
+        // Create original Student object
+        Student student1 = new Student(101, "Sudheer", address);
 
-		// Shallow Copy
-		Student s2 = (Student) s1.clone();
+        // Create shallow copy
+        Student student2 = student1.clone();
 
-		// Change s2 address
-		s2.address.city = "Vizag";
+        // Display before modification
+        System.out.println("Before Modification:");
+        System.out.println("Student 1 Name    : " + student1.name);
+        System.out.println("Student 1 City    : " + student1.address.city);
 
-		System.out.println("Student 1 Address : " + s1.address.city);
-		System.out.println("Student 2 Address : " + s2.address.city);
-	}
+        System.out.println("Student 2 Name    : " + student2.name);
+        System.out.println("Student 2 City    : " + student2.address.city);
+
+        System.out.println("--------------------------------");
+
+        // Modify the nested Address object
+        student2.address.city = "Hyderabad";
+
+        // Display after modification
+        System.out.println("After Modification:");
+        System.out.println("Student 1 Name    : " + student1.name);
+        System.out.println("Student 1 City    : " + student1.address.city);
+
+        System.out.println("Student 2 Name    : " + student2.name);
+        System.out.println("Student 2 City    : " + student2.address.city);
+
+        System.out.println("--------------------------------");
+
+        // Check references
+        System.out.println("Student objects same? : " + (student1 == student2));
+        System.out.println("Address objects same? : " + (student1.address == student2.address));
+    }
 }
+//
+//------------------------------------------------------------------------------------------------------------------------------------
+//package ShallowCopyAndDeepCopy;
+//
+//class Address implements Cloneable {
+//
+//    String city;
+//
+//    Address(String city) {
+//        this.city = city;
+//    }
+//
+//    // Deep copy of Address
+//    @Override
+//    protected Address clone() throws CloneNotSupportedException {
+//        return (Address) super.clone();
+//    }
+//}
+//
+//class Student implements Cloneable {
+//
+//    int number;
+//    String name;
+//    Address address;
+//
+//    Student(int number, String name, Address address) {
+//        this.number = number;
+//        this.name = name;
+//        this.address = address;
+//    }
+//
+//    // Deep Copy
+//    @Override
+//    protected Student clone() throws CloneNotSupportedException {
+//
+//        Student copy = (Student) super.clone();
+//
+//        // Create a separate Address object
+//        copy.address = address.clone();
+//
+//        return copy;
+//    }
+//}
+//
+//public class DeepCopy {
+//
+//    public static void main(String[] args) throws CloneNotSupportedException {
+//
+//        // Create Address object
+//        Address address = new Address("Vizag");
+//
+//        // Create original Student
+//        Student student1 = new Student(101, "Sudheer", address);
+//
+//        // Create Deep Copy
+//        Student student2 = student1.clone();
+//
+//        // Before modification
+//        System.out.println("Before Modification:");
+//
+//        System.out.println("Student 1 Name : " + student1.name);
+//        System.out.println("Student 1 City : " + student1.address.city);
+//
+//        System.out.println("Student 2 Name : " + student2.name);
+//        System.out.println("Student 2 City : " + student2.address.city);
+//
+//        System.out.println("--------------------------------");
+//
+//        // Modify Student 2's Address
+//        student2.address.city = "Hyderabad";
+//
+//        // After modification
+//        System.out.println("After Modification:");
+//
+//        System.out.println("Student 1 Name : " + student1.name);
+//        System.out.println("Student 1 City : " + student1.address.city);
+//
+//        System.out.println("Student 2 Name : " + student2.name);
+//        System.out.println("Student 2 City : " + student2.address.city);
+//
+//        System.out.println("--------------------------------");
+//
+//        // Check references
+//        System.out.println("Student objects same? : " + (student1 == student2));
+//        System.out.println("Address objects same? : " + (student1.address == student2.address));
+//    }
+//}
