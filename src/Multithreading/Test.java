e770072eb7927bb28155e3abad09bf4f09813396
Sudheer-77 <sudheerdash24@gmail.com>
2026-09-17 09:26:33 +0530

    setname ,getname

---
+++ src/Multithreading/Test.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Child Thread Name: " + Thread.currentThread().getName());
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+        MyThread t = new MyThread();
+
+        System.out.println("Default Thread Name: " + t.getName());
+
+        t.setName("MyChildThread");
+        System.out.println("After Changing Name: " + t.getName());
+
+        t.start();
+
+        System.out.println("Main Thread Name: " + Thread.currentThread().getName());
+    }
+}
